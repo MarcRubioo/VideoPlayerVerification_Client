31ea28b6566348bd38d23163a96e25b64a4c5496
MarcRubioo <marc.rubio@institutvidreres.cat>
2023-10-30 12:08:50 +0100

    Splash i binding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "cat.insvidreres.ima.videoplayerverification_client"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "cat.insvidreres.ima.videoplayerverification_client"
@@ -32,6 +32,10 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        dataBinding = true
     }
 }
 
---
+++ app/src/main/java/cat/insvidreres/ima/videoplayerverification_client/SplashActivity.kt
@@ -0,0 +1,12 @@
+package cat.insvidreres.ima.videoplayerverification_client
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
